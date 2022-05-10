dd5abdb7b38834b55d3667580ae9aba9f79c74fe
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-10 13:45:59 -0300

    Atualização de arquivo

--- index.js
+++ index.js
@@ -5,45 +5,24 @@ function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
   const arrayResultados = [];
   let temp;
-  while((temp = regex.exec(texto)) != null) {
+  while((temp = regex.exec(texto)) !== null) {
     arrayResultados.push({ [temp[1]]: temp[2] })
   }
-  return arrayResultados;
+  return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
 }
 
 function trataErro(erro) {
-  throw new Error(chalk.red(erro.code,'não há arquivo no caminho'));
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch(erro) {
     trataErro(erro);
   }
 }
 
-pegaArquivo('./arquivos/texto1.md');
-
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises
-//   .readFile(caminhoDoArquivo, encoding)
-//   .then((texto) => chalk.green(console.log(texto)))
-//   .catch((erro) => trataErro(erro))
-// }
-
-// function pegaArquivo (caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   })
-// }
-
-
+module.exports = pegaArquivo;
